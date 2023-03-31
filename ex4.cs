5293f1da8ecf3f69d5a8ba96af7e0107c81e0ed5
Mandryty <119812501+Mandryty@users.noreply.github.com>
2023-03-30 21:12:59 -0300

    Add files via upload

---
+++ ex4.cs
@@ -0,0 +1,18 @@
+﻿namespace exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicando;
+
+            Console.WriteLine("Informe o número desejado: ");
+            multiplicando = Convert.ToInt32(Console.ReadLine());
+
+            for (int multiplicador = 0; multiplicador <= 9; multiplicador++)
+            {
+                Console.WriteLine(string.Format("{0}*{1}={2}", multiplicando, multiplicador, multiplicando*multiplicador));
+            }
+        }
+    }
+}
